122de6a85008ff093831ec520b98b61625124abb
LorenzLamm <lorenz.lamm@googlemail.com>
2025-01-29 13:56:18 +0100

    add more cli commands

--- tests/test_clis.sh
+++ tests/test_clis.sh
@@ -2,7 +2,10 @@
 # Example command-line operations
 echo "Running membrain"
 membrain
+membrain segment
+membrain train
 echo "Running membrain_pick"
 membrain_pick
+membrain_pick train
 echo "Running membrain_stats"
 membrain_stats
